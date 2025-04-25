da730377dc0beb6acdbcfae47884778b17c56bca
KareemEhab <kareem8002010@gmail.com>
2025-04-26 01:33:52 +0300

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
 
-import { FormComponent } from './components/form/form.component';
-import { ProductsComponent } from './components/products/products.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormComponent, ProductsComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  product = {};
-  getProductData(data: any) {
-    this.product = data;
-  }
-}
+export class AppComponent {}
