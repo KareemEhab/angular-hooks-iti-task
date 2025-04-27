edebcd219e8b68e665940d4596d7002848e2686f
KareemEhab <kareem8002010@gmail.com>
2025-04-27 20:42:59 +0300

    Products page

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { FormComponent } from '../../components/form/form.component';
+import { ProductsCardsComponent } from '../../components/products-cards/products-cards.component';
 
 @Component({
   standalone: true,
   selector: 'app-products',
-  imports: [],
+  imports: [FormComponent, ProductsCardsComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
 })
-export class ProductsComponent {}
+export class ProductsComponent {
+  product = {};
+  getProductData(data: any) {
+    this.product = data;
+  }
+}
